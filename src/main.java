2edada2a6474a7e87c9aa69a4eec84d3614bc35c
Nikita Reshetnikov <60236568+Mortiferus@users.noreply.github.com>
2020-11-30 18:58:09 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPriceKpks;
-        ticketPriceKpks = 20_000_00;
+        int ticketPriceKpks = 20_000_00;
         int bonusMiles = ticketPriceKpks / 20 / 100;
         System.out.println(bonusMiles);
     }
